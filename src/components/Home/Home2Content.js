b5e0724a513077dc58dfabb5affd1238137d7899
nguyenha211 <103014400+nguyenha211@users.noreply.github.com>
2022-10-08 16:10:44 +0700

    taoa thich thi tao thay doi

---
+++ src/components/Home/Home2Content.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Home2Content(props) {
+  var { count } = props;
+  return <div>you click {count} times</div>;
+}
--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function HomeContent() {
-  return <div>Content</div>;
+  var [show, isShow] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          isShow(false);
+        }}
+      >
+        Show
+      </button>
+      <button
+        onClick={function () {
+          isShow(true);
+        }}
+      >
+        Unshow
+      </button>
+
+      <div hidden={show}>You see me</div>
+    </div>
+  );
 }
