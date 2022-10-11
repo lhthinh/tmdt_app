64420d76dd89183648eb28ddc229a02d10c5d39a
nguyenha211 <103014400+nguyenha211@users.noreply.github.com>
2022-10-11 15:47:46 +0700

    test

--- src/components/Home/Home2Content.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Home2Content(props) {
-  var { count } = props;
-  return <div>you click {count} times</div>;
-}
--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -1,25 +1,129 @@
+import { Carousel, Col, Row, Card } from "antd";
 import React, { useState } from "react";
+import hinh from "../../assets/image/hinh1.jpg";
+
+import "./style.css";
 
 export default function HomeContent() {
-  var [show, isShow] = useState(false);
+  const contentStyle = {
+    height: "500px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
+  const row_home_1 = {
+    marginLeft: "180px",
+  };
+  const body_title_styles_1 = {
+    marginTop: "20px",
+    fontSize: "30px",
+    marginLeft: "180px",
+  };
+  const blog_1 = {};
+  const linksGroupTitle = {};
+  const linksGroupProduct = {};
   return (
-    <div>
-      <button
-        onClick={() => {
-          isShow(false);
-        }}
-      >
-        Show
-      </button>
-      <button
-        onClick={function () {
-          isShow(true);
-        }}
-      >
-        Unshow
-      </button>
+    <>
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle} className="contentStyle">
+            <img src={hinh}></img>
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
 
-      <div hidden={show}>You see me</div>
-    </div>
+      <Row style={row_home_1}>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{
+              width: 350,
+            }}
+            cover={<img src={hinh} />}
+          ></Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{
+              width: 350,
+            }}
+            cover={<img src={hinh} />}
+          ></Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{
+              width: 350,
+            }}
+            cover={<img src={hinh} />}
+          ></Card>
+        </Col>
+      </Row>
+
+      <Row style={body_title_styles_1}>
+        <Col span={6} className="links-title">
+          <p style={blog_1}>From The Blog</p>
+        </Col>
+
+        <Col span={12} style={linksGroupTitle} className="links-group-title">
+          <a href="https://#">Feartured</a>
+          <a href="https://#">On Sale</a>
+          <a href="https://#">Top Rate</a>
+        </Col>
+      </Row>
+      <Row style={body_title_styles_1}>
+        <Col span={6}>
+          <Card
+            hoverable
+            style={{
+              width: 350,
+            }}
+            cover={<img src={hinh} />}
+          ></Card>
+        </Col>
+
+        <Col span={12} style={linksGroupProduct} class="links-group-product">
+          <Col span={4}>
+            <Card
+              hoverable
+              style={{
+                width: 350,
+              }}
+              cover={<img src={hinh} />}
+            ></Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              style={{
+                width: 350,
+              }}
+              cover={<img src={hinh} />}
+            ></Card>
+          </Col>
+          <Col span={4}>
+            <Card
+              hoverable
+              style={{
+                width: 350,
+              }}
+              cover={<img src={hinh} />}
+            ></Card>
+          </Col>
+        </Col>
+      </Row>
+    </>
   );
 }
