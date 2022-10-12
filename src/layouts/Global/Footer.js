b93e299ca74d5a5b378945aea7276c4e9fbbac8b
chinh le <levinhchinh01062001@gmail.com>
2022-10-12 21:00:21 +0700

    thay doi footer

--- src/layouts/Global/Footer.js
+++ src/layouts/Global/Footer.js
@@ -1,5 +1,34 @@
+import { Col, Row } from "antd";
 import React from "react";
-
+import "./footer.scss";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-title">
+        <Row>
+          <Col span={12} className="title">
+            Sign up to Newsletter
+          </Col>
+          <Col span={12} className="input-footer">
+            <input className="boder-footer" type="text"></input>
+            <button className="button-signup-footer">SignUp</button>
+          </Col>
+        </Row>
+      </div>
+      <div className="footer-content">
+        <Row>
+          <Col span={12} className="footer-info">
+            <p className="name-info">ASICC</p>
+            <h1>+84098763321</h1>
+            <p>
+              <b>Contact info</b>
+            </p>
+            <p>Lầu 5, 35 Nguyễn Văn Tráng, Phường Bến Nghé, Quận 1, Tp.HCM</p>
+            <p>icon</p>
+          </Col>
+          <Col span={12} className="footer-intro"></Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
--- src/layouts/Global/Header.js
+++ src/layouts/Global/Header.js
@@ -1,5 +1,66 @@
+import { Col, Row, Space } from "antd";
+import Column from "antd/lib/table/Column";
 import React from "react";
-
+import { Carousel } from "antd";
+import "./header.scss";
 export default function Header() {
-  return <div>Header</div>;
+  const contentStyle = {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+    borderRadius: "10px",
+  };
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+  return (
+    <div>
+      <div className="header">
+        <Space>
+          <div>Store Locator</div>
+          <div>Track Your Oder</div>
+          <div>Shop</div>
+          <div>My Account</div>
+        </Space>
+      </div>
+      <hr />
+      <div>
+        <Row>
+          <Col span={6}>
+            <h1 className="title-text">ASICC</h1>
+          </Col>
+          <Col span={12} offset={1}>
+            <input className="boder" type={"text"} />
+          </Col>
+        </Row>
+      </div>
+      <div className="center">
+        <Space>
+          <div>Home</div>
+          <div>About</div>
+          <div>Blog</div>
+          <div>Contact Us</div>
+        </Space>
+      </div>
+      <div>
+        {" "}
+        <Carousel afterChange={onChange}>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
