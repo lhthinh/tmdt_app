f4f523d51367fe1f9c92e5b88c9de3c91a6f0561
lhthinh <thinhlh7@fpt.com.vn>
2022-10-13 10:15:07 +0700

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "antd/dist/antd.min.css";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import { Provider } from "react-redux";
 
 function App() {
   return (
---
+++ src/features/counter/counter-slice.js
@@ -0,0 +1,18 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value++;
+    },
+  },
+});
+
+export const { increment } = counterSlice.actions;
+export default counterSlice.reducer;
